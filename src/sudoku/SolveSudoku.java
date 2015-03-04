ed3eddddd2d401ef1f09585333b697902ce749dd
Anton Schölin <antonscholin@gmail.com>
2015-03-04 17:07:50 +0100

    So close...

--- src/sudoku/SolveSudoku.java
+++ src/sudoku/SolveSudoku.java
@@ -4,9 +4,9 @@ public class SolveSudoku {
 	
 	public static void main(String[] args) {
 		Sudoku s = new Sudoku();
-		System.out.println(s);
+		System.out.println(s.toString());
 		if(s.solve()){
-			System.out.println(s);
+			System.out.println(s.toString());
 		}else{
 			System.out.println("Sudoko kan ej lösas");
 		}
--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -2,15 +2,25 @@ package sudoku;
 
 public class Sudoku {
 	int[][] board;
+	int counter = 0;
+	boolean solved=false;
 
 	public Sudoku() {
 		this.board = new int[9][9];
 
-		setValue(0,0,1);
-		setValue(0,1,2);
-		setValue(5,6,5);
-		
-		valid(5,5, getValue(0,0));
+//		 setValue(0, 0, 1);
+//		 setValue(0, 1, 2);
+//		 setValue(0, 2, 3);
+//		 setValue(1, 0, 5);
+//		 setValue(1, 1, 5);
+//		 setValue(1, 2, 6);
+//		 setValue(2, 0, 7);
+//		 setValue(2, 1, 8);
+//		 setValue(2, 2, 9);
+//		 setValue(0, 3, 1);
+//		 setValue(0, 3, 4);
+//		 setValue(7, 0, 8);
+//		 setValue(8, 2, 9);
 	}
 
 	/**
@@ -25,8 +35,11 @@ public class Sudoku {
 	 * @return
 	 */
 	public boolean setValue(int i, int j, int value) {
-		board[i][j] = value;
-		return true;
+		if (value >= 0 && value <= 9) {
+			board[i][j] = value;
+			return true;
+		} else
+			return false;
 	}
 
 	/**
@@ -56,109 +69,123 @@ public class Sudoku {
 		}
 		return sb.toString();
 	}
-	
-	public boolean solve(){
-		return solve(0,0);
+
+	public boolean solve() {
+		return solve(0, 0);
 	}
 
 	private boolean solve(int row, int col) {
+		System.out.println(counter);
+		counter++;
+//		if(counter==200){
+//			System.out.println("Finally...");
+//		}
+		boolean solveReturn = false;
 
-		// getValue från i,j
-		// kolla om cellen är ifylld av användaren
-		// ifylld: checka giltighet (rad, kolumn, ruta) returnera falskt om
-		// ogiltig, annars gå vidare
-		// ej ifylld: fyll med 1-9. Checka giltighet. Om giltig, gör rekursivt
-		// anrop till nästa cell.
 		int v = getValue(row, col);
 		if (v != 0) {
+			setValue(row, col, 0);
 			if (valid(row, col, v)) {
+				setValue(row, col, v);
 				if (row < 8 && col < 8) {
-					return solve(row, col + 1);
+					solveReturn = solve(row, col + 1);
 				} else if (row < 8 && col == 8) {
-					return solve(row + 1, 0);
+					solveReturn = solve(row + 1, 0);
 				} else {
-					return true;
+					solveReturn = true;
 				}
 			} else {
-				setValue(row,col,0);
-				return false;
+				solveReturn = false;
 			}
 		} else {
-			/*
-			Måste hitta annat sätt att öka k!
-			*/
-			for (int k = 1; k <= 9; k++) {
-				if (valid(row, col, k)) {
-					if (row < 8 && col < 8) {
-						setValue(row,col,k);
-						return solve(row, col + 1);
-					} else if (row < 8 && col == 8) {
-						setValue(row,col,k);
-						return solve(row + 1, 0);
+			for (int val = 1; val <= 9; val++) {
+				if(solved==true)
+					break;
+				if (valid(row, col, val)) {
+					if (col < 8) {
+						setValue(row, col, val);
+						solveReturn = solve(row, col + 1);
+					} else if (row < 8) {
+						setValue(row, col, val);
+						solveReturn = solve(row + 1, 0);
 					} else {
+						setValue(row, col, val);
+						solved=true;
 						return true;
 					}
-				}else{
-					setValue(row,col,0);
-					return false;
+				} else {
+					if (val == 9) {
+						setValue(row, col, 0);
+						solveReturn = false;
+					}
 				}
 			}
 		}
-		return false;
+		return solveReturn;
 	}
 
-	private boolean valid (int i, int j, int value) {
+	private boolean valid(int i, int j, int value) {
+		// Can always set value to zero
+		if (value == 0)
+			return true;
+
 		int[][] set = getSetToCheck(i, j, value);
-			
-		System.out.print("Row: ");
-		for (int a = 0; a < 8; a++) {
-			System.out.print(set[0][a] + " ");
+
+		// System.out.print("Row: ");
+		for (int val : set[0]) {
+			if (val == value)
+				return false;
+			// System.out.print(set[0][a] + " ");
 		}
-		System.out.print("\n");
-		
-		System.out.print("Col: ");
-		for (int a = 0; a < 8; a++) {
-			System.out.print(set[1][a] + " ");
+		// System.out.print("\n");
+
+		// System.out.print("Col: ");
+		for (int val : set[1]) {
+			if (val == value)
+				return false;
+			// System.out.print(set[1][a] + " ");
 		}
-		System.out.print("\n");
-		
-		System.out.print("Box: ");
-		for (int a = 0; a < 8; a++) {
-			System.out.print(set[2][a] + " ");
+		// System.out.print("\n");
+
+		// System.out.print("Box: ");
+		for (int val : set[2]) {
+			if (val == value)
+				return false;
+			// System.out.print(set[2][a] + " ");
 		}
-		System.out.print("\n");
-					
-		return false;
+		// System.out.print("\n");
+
+		return true;
 	}
-	
-	private int[][] getSetToCheck (int row, int col, int value) {
-		
-		System.out.println(row + ", " + col);
-		
+
+	private int[][] getSetToCheck(int row, int col, int value) {
+
+		// System.out.println(row + ", " + col);
+
 		int[][] set = new int[3][9];
 		set[0] = new int[9];
 		set[1] = new int[9];
 		set[2] = new int[9];
-		
+
 		// Get the row array
 		for (int c = 0; c < 8; c++) {
 			set[0][c] = board[row][c];
 		}
-		
+
 		// Get the column array
 		for (int r = 0; r < 8; r++) {
 			set[1][r] = board[r][col];
 		}
-		
+
 		// Get the box array
 		for (int r = 0; r < 3; r++) {
 			for (int c = 0; c < 3; c++) {
-				set[2][c + r*3] = board[row/3 + r][col/3 + c];
+				set[2][c + r * 3] = board[3 * (row / 3) + r][3 * (col / 3) + c];
 			}
 		}
-		
+
 		return set;
-		
+
 	}
 
 }
