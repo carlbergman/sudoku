9442615b2059401ccb5fe2b67553a58f35a1f859
Anton Schölin <antonscholin@gmail.com>
2015-03-05 20:23:57 +0100

    asdasd

--- src/sudoku/SolveSudoku.java
+++ src/sudoku/SolveSudoku.java
@@ -4,14 +4,14 @@ public class SolveSudoku {
 	
 	public static void main(String[] args) {
 		Sudoku s = new Sudoku();
-		System.out.println(s.toString());
-		if(s.solve()){
-			System.out.println(s.toString());
-		}else{
-			System.out.println("Sudoko kan ej lösas");
-		}
+//		System.out.println(s.toString());
+//		if(s.solve()){
+//			System.out.println(s.toString());
+//		}else{
+//			System.out.println("Sudoko kan ej lösas");
+//		}
 		
-//		SudokuGUI sgui = new SudokuGUI(s);		
+		SudokuGUI sgui = new SudokuGUI(s);		
 		
 	}
 	
